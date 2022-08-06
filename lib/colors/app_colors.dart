ef8f93bf4b987c393c2f282deddef9aa9b8f5c80
tauptaup <104350955+tauptaup@users.noreply.github.com>
2022-08-06 23:42:36 +0600

    Create app_colors.dart

---
+++ lib/colors/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const mainGreen = Color(0xFF34C759);
+  static const mainRed = Color(0xFFFF3B30);
+  static const mainCanvas = Color(0xFFF7F6F2);
+  static const mainBlue = MaterialColor(0xFF007AFF, {
+    50: Color(0xFF007AFF),
+    100: Color(0xFF007AFF),
+    200: Color(0xFF007AFF),
+    300: Color(0xFF007AFF),
+    400: Color(0xFF007AFF),
+    500: Color(0xFF007AFF),
+    600: Color(0xFF007AFF),
+    700: Color(0xFF007AFF),
+  });
+}
