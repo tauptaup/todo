cc767bec3c894528304ef391bd9faa2b5cd657a6
tauptaup <tauptaup@list.ru>
2022-08-06 19:56:19 +0600

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/zxc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zxc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Poi Poi Todo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
