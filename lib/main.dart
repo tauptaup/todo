d5e1e52aa18828022bd814fe6d2198e14e3a995f
tauptaup <104350955+tauptaup@users.noreply.github.com>
2022-08-07 17:51:59 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '/screens/home_screen.dart';
+import 'colors/app_colors.dart';
 import 'colors/themes.dart';
 
 void main() {
@@ -14,9 +15,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo',
-      themeMode: ThemeMode.system,
-      theme: MyThemes.lighTheme,
-      // darkTheme: MyThemes.darkTheme,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primaryColor: Color(0xFFF7F6F2),
+        accentColor: Colors.white,
+        canvasColor: AppColors.mainCanvas,
+        cardColor: Color(0xFF8E8E93),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.mainCanvas,
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleMedium: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              titleLarge: const TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: const ColorScheme.dark(),
+        canvasColor: Color(0xFF161618),
+        primaryColor: const Color(0xFF161618),
+        hintColor: Colors.white,
+        cardColor: Color(0xFF8E8E93),
+        accentColor: const Color(0xFF252528),
+        textTheme: ThemeData.dark().textTheme.copyWith(
+              bodyMedium: const TextStyle(color: Colors.white),
+              titleMedium: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              titleLarge: const TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+      ),
       home: HomeScreen(),
     );
   }
