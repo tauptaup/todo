f908cf3a026473a22e2e71a452d978b4398371bb
tauptaup <104350955+tauptaup@users.noreply.github.com>
2022-08-07 17:52:50 +0600

    Update themes.dart

--- lib/colors/themes.dart
+++ lib/colors/themes.dart
@@ -3,13 +3,5 @@ import 'package:flutter/material.dart';
 import 'app_colors.dart';
 
 class MyThemes {
-  static final lighTheme = ThemeData(
-    primarySwatch: AppColors.mainBlue,
-    canvasColor: AppColors.mainCanvas,
-  );
-  static final darkTheme = ThemeData(
-    canvasColor: Color(0xFFE5E5E5),
-    accentColor: Color(0xFF252528),
-    primaryColor: Color(0xFF161618),
-  );
+  // static final darkTheme =
 }
